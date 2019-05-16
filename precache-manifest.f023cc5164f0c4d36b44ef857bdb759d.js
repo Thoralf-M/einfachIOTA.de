79baf3477333c3176e94e7abd21bde5bccb99c75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-16 10:49:58 +0000

    Deploy einfachiota/einfachIOTA.de to github.com/einfachiota/einfachIOTA.de.git:gh-pages

---
+++ precache-manifest.f023cc5164f0c4d36b44ef857bdb759d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d691a91bb9e4aa47d5a",
+    "url": "/einfachIOTA.de/js/about.09cd1052.js"
+  },
+  {
+    "revision": "05ebbdecdc4640728670",
+    "url": "/einfachIOTA.de/css/app.9b9db59a.css"
+  },
+  {
+    "revision": "05ebbdecdc4640728670",
+    "url": "/einfachIOTA.de/js/app.aa229d90.js"
+  },
+  {
+    "revision": "c6250f43415bccc620ab",
+    "url": "/einfachIOTA.de/css/chunk-vendors.af67b8ba.css"
+  },
+  {
+    "revision": "c6250f43415bccc620ab",
+    "url": "/einfachIOTA.de/js/chunk-vendors.8b8b30f3.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/einfachIOTA.de/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/einfachIOTA.de/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "97ae26195c5455c421c43568223c211b",
+    "url": "/einfachIOTA.de/img/logo.97ae2619.svg"
+  },
+  {
+    "revision": "7ec09dc2dfae6b60f53861c0316c0285",
+    "url": "/einfachIOTA.de/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/einfachIOTA.de/robots.txt"
+  }
+];
